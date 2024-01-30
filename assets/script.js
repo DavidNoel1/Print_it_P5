d662403208aea5dbbf906283f6c54b974f0067cd
David Noel <walipululu@gmail.com>
2024-01-30 02:34:13 +0100

    event listener sur les deux flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,8 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+const arrowright = document.querySelector('.arrow_right');
+const arrowleft = document.querySelector('.arrow_left');
+
